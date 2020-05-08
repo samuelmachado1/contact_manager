ac7d504bd5d8c7a32b15a6a2130985a12da16c32
Samuel Estrella <samuelmachado1@gmail.com>
2020-05-07 22:12:26 -0300

    Configuring NodeJS and database connection

---
+++ server/app.ts
@@ -0,0 +1,21 @@
+import * as express from 'express';
+
+import database from './db';
+
+class App{
+    public app: express.Application;
+    private database: database;
+     
+    constructor(){
+        this.app = express();
+        this.database = new database();
+        this.database.createConnection();
+        this.routes();
+    }
+
+    routes(){
+        this.app.route('/').get((req, res) => res.status(200).json({"result": "Hello World"}) );
+    }
+}
+
+export default new App();
---
+++ server/db.ts
@@ -0,0 +1,26 @@
+import * as mongoose from 'mongoose';
+import e = require('express');
+
+class DataBase{
+   
+    private dburl = 'mongodb://127.0.0.1/contact-manager';
+    private dbconnection;
+
+    constructor(){}
+
+    createConnection(){
+        mongoose.connect(this.dburl);
+        this.logger(this.dburl);
+    }
+
+    logger(uri){
+        this.dbconnection = mongoose.connection;
+        this.dbconnection.on('connected', () => console.log("mongoose está conectado!"))
+        this.dbconnection.on('error', error => console.error.bind(console, "erro na conexão: " + error));
+
+    }
+
+
+}
+
+export default DataBase;
